f6b561eda11f3cc35a3b4f67583e9703c7f5ddfb
George Wilson <george@wils.online>
2018-07-19 15:09:04 +1000

    Remove 'sv' from parse function names
    This is a hangover from sv-0.1 (before this library was split out)
    and it is no longer necessary.

--- src/Data/Svfactor/Parse.hs
+++ src/Data/Svfactor/Parse.hs
@@ -8,10 +8,10 @@ Portability : non-portable
 -}
 
 module Data.Svfactor.Parse (
-  parseSv
-, parseSv'
-, parseSvFromFile
-, parseSvFromFile'
+  parse
+, parse'
+, parseFromFile
+, parseFromFile'
 , separatedValues
 
 , module Data.Svfactor.Parse.Options
@@ -42,30 +42,30 @@ import Data.Svfactor.Parse.Options
 -- | Parse a 'ByteString' as an Sv.
 --
 -- This version uses Trifecta, hence it assumes its input is UTF-8 encoded.
-parseSv :: ParseOptions ByteString -> ByteString -> Either ByteString (Sv ByteString)
-parseSv = parseSv' trifecta
+parse :: ParseOptions ByteString -> ByteString -> Either ByteString (Sv ByteString)
+parse = parse' trifecta
 
 -- | Parse some text as an Sv.
 --
 -- This version lets you choose which parsing library to use by providing an
 -- 'SvParser'. Common selections are 'trifecta' and 'attoparsecByteString'.
-parseSv' :: SvParser s -> ParseOptions s -> s -> Either s (Sv s)
-parseSv' svp opts s =
+parse' :: SvParser s -> ParseOptions s -> s -> Either s (Sv s)
+parse' svp opts s =
   let enc = view encodeString opts
   in  first enc $ runSvParser svp opts s
 
 -- | Load a file and parse it as an 'Sv'.
 --
 -- This version uses Trifecta, hence it assumes its input is UTF-8 encoded.
-parseSvFromFile :: MonadIO m => ParseOptions ByteString -> FilePath -> m (Either ByteString (Sv ByteString))
-parseSvFromFile = parseSvFromFile' trifecta
+parseFromFile :: MonadIO m => ParseOptions ByteString -> FilePath -> m (Either ByteString (Sv ByteString))
+parseFromFile = parseFromFile' trifecta
 
 -- | Load a file and parse it as an 'Sv'.
 --
 -- This version lets you choose which parsing library to use by providing an
 -- 'SvParser'. Common selections are 'trifecta' and 'attoparsecByteString'.
-parseSvFromFile' :: MonadIO m => SvParser s -> ParseOptions s -> FilePath -> m (Either s (Sv s))
-parseSvFromFile' svp opts fp =
+parseFromFile' :: MonadIO m => SvParser s -> ParseOptions s -> FilePath -> m (Either s (Sv s))
+parseFromFile' svp opts fp =
   let enc = view encodeString opts
   in  liftIO (first enc <$> runSvParserFromFile svp opts fp)
 

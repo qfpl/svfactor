1c6a54378b8b230a3a6f171cb113345807d1d182
Dave Laing <dave.laing.80@gmail.com>
2018-11-23 11:38:34 +1000

    Various fixes and updates for Hydra

--- ci/jobsets.nix
+++ ci/jobsets.nix
@@ -18,7 +18,7 @@
             "keepnr": 5,
             "inputs": {
                 "svfactor": { "type": "git", "value": "https://github.com/qfpl/svfactor.git master", "emailresponsible": false },
-                "nixpkgs": { "type": "git", "value": "https://github.com/NixOS/nixpkgs.git release-18.03", "emailresponsible": false }
+                "nixpkgs": { "type": "git", "value": "https://github.com/NixOS/nixpkgs.git release-18.09", "emailresponsible": false }
             }
         }
     }
